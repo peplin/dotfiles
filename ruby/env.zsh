ba0966a2d4f890a2f2bd5969ffae18f9bce15397
Christopher Peplin <peplin@bueda.com>
2010-05-05 00:44:21 -0400

    Set enough gem environment settings for homedir gems to work.

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -1,3 +1,5 @@
-export PATH=$PATH:/var/lib/gems/1.8/bin:$HOME/.gem/ruby/1.8/bin
 export IRBRC=$HOME/.irbrc
 export RUBYOPT="rubygems"
+export GEM_HOME=$HOME/.gem
+export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
+export PATH=$PATH:/var/lib/gems/1.8/bin:$GEM_HOME/bin
