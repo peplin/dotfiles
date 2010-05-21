127ed84aeb0d5f3d56fcfb7793fccb2ef580b03d
Christopher Peplin <peplin@bueda.com>
2010-05-21 14:14:26 -0400

    Add first level subdirectories in ~/bin to PATH.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,7 +6,14 @@ fi
 
 export EDITOR='vi'
 export PAGER="less"
+
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+
+# add all first level subdirectories in ~/bin to PATH
+for DIR in `find ~/bin/ -maxdepth 1 -type d`; do                            
+    export PATH=$PATH:$DIR
+done
+
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
