ccbcf8668845073b681cd0a424ee10674582e5c4
Christopher Peplin <peplin@bueda.com>
2010-10-06 12:34:02 -0400

    Start using rvm.

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -1,5 +1,1 @@
 export IRBRC=$HOME/.irbrc
-export RUBYOPT="rubygems"
-export GEM_HOME=$HOME/.gem
-export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
-export PATH=$PATH:/var/lib/gems/1.8/bin:$GEM_HOME/bin
