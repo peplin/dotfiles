ac7bfdd3a72717e1e4bc13f255617c2414b1a711
Christopher Peplin <peplin@bueda.com>
2010-11-02 11:04:32 -0400

    Add remote shell shortcut for Rails and Django.
    (rails helper and idea courtesy http://github.com/robbyrussell/oh-my-zsh)

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -5,3 +5,8 @@ alias rc='./manage.py shell'
 validatejson() {
     cat $1 | python -m simplejson.tool
 }
+
+function remote_shell() {
+    environment=$3 || "PRODUCTION" 
+    /usr/bin/env ssh $1 "( DEPLOYMENT_TYPE=$environment cd $2 && env/bin/python ./manage.py shell )"
+}
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -2,3 +2,7 @@ alias rc='rails console'
 alias rs='rails server'
 alias tlog='tail -f log/development.log'
 alias s="ps aux | grep \"[r]uby\" | grep \"rails server\" || echo \"You're not running any, dawg.\""
+
+function remote_console() {
+  /usr/bin/env ssh $1 "( cd $2 && ruby script/console production )"
+}
