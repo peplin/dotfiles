c1caac3e3b5c022dfdddc2e6d58149eac3bb118a
Christopher Peplin <peplin@bueda.com>
2011-01-05 12:00:25 -0500

    Don't search through coverage output.

--- python/env.zsh
+++ python/env.zsh
@@ -1,6 +1,5 @@
 export PYTHONSTARTUP=$HOME/.pythonrc
 export PYTHONPATH=$PYTHONPATH:$PROJECTS/bueda/ops
-export PYTHONUSERDIR=$HOME/.dotfiles/python/startup
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
