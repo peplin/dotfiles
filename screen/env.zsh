0d5ac306e93c7e939e837f448e4febe4ad9504ea
Christopher Peplin <peplin@bueda.com>
2011-02-05 23:59:56 +0000

    Persistent SSH agent forwarding in screen sessions.

--- screen/env.zsh
+++ screen/env.zsh
@@ -1,3 +1,6 @@
+test $SSH_AUTH_SOCK && [[ $SSH_AUTH_SOCK != "/tmp/ssh-agent-$USER-screen" ]] && \
+    ln -sf "$SSH_AUTH_SOCK" "/tmp/ssh-agent-$USER-screen"
+
 if [ $SSH_TTY ] && [ ! $WINDOW ]; then
   SCREENLIST=`screen -ls | grep 'Attached'`
   if [ $? -eq "0" ]; then
