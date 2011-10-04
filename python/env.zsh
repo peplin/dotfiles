2f390d7e09afaafc35d9ec80b6bc7125535bc50a
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-10-04 14:49:06 -0400

    Add PROJECT_HOME for new version of virtualenvwrapper.

--- python/env.zsh
+++ python/env.zsh
@@ -2,6 +2,7 @@ export PYTHONSTARTUP=$HOME/.dotfiles/python/startup/startup.py
 export PYTHONPATH=$PYTHONPATH:$PROJECTS/bueda/ops
 export PYTHONUSERDIR=$HOME/.dotfiles/python/startup
 
+export PROJECT_HOME=$PROJECTS
 export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
