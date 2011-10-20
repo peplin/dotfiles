cc620767498b0023cfe5d08f0046bc9aafbf5d23
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-10-20 10:46:31 -0400

    Update JDK path on OS X.

--- java/env.zsh
+++ java/env.zsh
@@ -2,4 +2,4 @@ export M2_HOME=$HOME/bin/apache-maven-3.0.3/
 export MAVEN_OPTS=-Xmx1512M
 export ANT_OPTS="-Xmx512m"
 export JAVA_OPTS="-Xmx512m"
-export JAVA_HOME="/usr/lib/jvm/default-java"
+export JAVA_HOME="/Library/Java/Home"
