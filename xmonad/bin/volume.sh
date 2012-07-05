3d2b82822e0a5fb497192aea0d5ed478e7b60b0a
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-07-04 21:37:29 -0400

    Swap volume scripts.

--- xmonad/bin/volume.sh
+++ xmonad/bin/volume.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
-DIRECTION=$1
-DIR=$(cd $(dirname "$0"); pwd)
 
-amixer -q set Master 5%$DIRECTION
+vol=$(amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") \
+    { print "MM" } else { print $2/10 }}' | head -n 1 | cut -d . -f 1)
+
+case $vol in
+  0)     bar='[----------]' ;;
+  1)   bar='[#---------]' ;;
+  2)   bar='[##--------]' ;;
+  3)   bar='[###-------]' ;;
+  4)   bar='[####------]' ;;
+  5)  bar='[#####-----]' ;;
+  6)  bar='[######----]' ;;
+  7)  bar='[#######---]' ;;
+  8)  bar='[########--]' ;;
+  9)  bar='[#########-]' ;;
+  10) bar='[##########]' ;;
+  MM) bar='[---Muted---]' ;;
+  *)  bar='[----------]' ;;
+esac
+
+echo $bar
