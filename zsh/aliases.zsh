473e31b9a65a504131ce019a611fb510266c6f02
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-05 15:13:10 -0400

    Remove unused Gnome junk.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,9 +4,6 @@ alias reload='. ~/.zshrc'
 
 alias markdown2pdf='/usr/bin/markdown2pdf --template ~/Dropbox/reference/markdown.tex'
 
-alias alert='notify-send -i gnome-terminal \
-    "[$?] $(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/;\s*alert$//'\'')"'
-
 calc() { awk "BEGIN{ print $* }" ;}
 
 urlencode() { echo `perl -MURI::Escape -e "print uri_escape('$1');"` ;}
