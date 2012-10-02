1ac733077eedecb32c5b8673f19b1f0f32984b41
Christopher Peplin <cpeplin@ford.com>
2012-10-02 09:49:27 -0400

    Load both 1.9 and 1.8 Ruby gems.

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -1,3 +1,3 @@
 export IRBRC=$HOME/.irbrc
-export PATH=$PATH:$HOME/.gem/ruby/1.9.1/bin
+export PATH=$PATH:$HOME/.gem/ruby/1.9.1/bin:$HOME/.gem/ruby/1.8/bin
 export GEM_HOME="$(ruby -rubygems -e 'puts Gem.user_dir')"
