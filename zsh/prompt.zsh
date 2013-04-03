dab127c729a44f509ae4c83d3b91f054dc6fe0c6
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-04-03 14:57:21 -0400

    Non-bold green for git prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -29,7 +29,7 @@ git_dirty() {
     else
         if [[ "$st" =~ ^nothing ]]
         then
-            echo "%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+            echo "%{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
         else
             echo "%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
         fi
