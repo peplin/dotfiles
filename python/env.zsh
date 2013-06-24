8e2228e760a0f3d5792f23244fe589ca8b4b9ff8
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-06-24 18:43:48 -0400

    Remove unused package from PYTHONPATH.

--- python/env.zsh
+++ python/env.zsh
@@ -1,5 +1,5 @@
 export PYTHONSTARTUP=$HOME/.dotfiles/python/startup/startup.py
-export PYTHONPATH=$PYTHONPATH:$PROJECTS/bueda/ops
+export PYTHONPATH=$PYTHONPATH
 export PYTHONUSERDIR=$HOME/.dotfiles/python/startup
 
 export PROJECT_HOME=$PROJECTS
