b47c8d54e6cfb9f76bdbd3168ed5b947cb70d651
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-06-09 22:19:09 -0400

    Update battery script to new kernel.

--- bin/batterycheck.sh
+++ bin/batterycheck.sh
@@ -15,7 +15,7 @@ ICON="/usr/share/icons/gnome/48x48/devices/battery.png"
 BATTERY_PATH="/sys/class/power_supply/$BAT"
 
 if [ -e "$BATTERY_PATH" ]; then
-    MAX_BATTERY=$(cat $BATTERY_PATH/charge_full)
+    MAX_BATTERY=$(cat $BATTERY_PATH/energy_full)
     LOW_BATTERY=$(($LOW_BATTERY * $MAX_BATTERY / 100))
     CRITICAL_BATTERY=$(($CRITICAL_BATTERY * $MAX_BATTERY/100))
 
@@ -23,9 +23,10 @@ if [ -e "$BATTERY_PATH" ]; then
     if [ "$PRESENT" = "1" ]; then
 
         STATE=$(cat "$BATTERY_PATH/status")
-        CAPACITY=$(cat "$BATTERY_PATH/charge_now")
+        CAPACITY=$(cat "$BATTERY_PATH/energy_now")
         PERCENTAGE=$(cat "$BATTERY_PATH/capacity")
 
+        echo "Percentage: $PERCENTAGE"
         if [ "$CAPACITY" -lt "$LOW_BATTERY" ] && [ "$STATE" = "Discharging" ]; then
             DISPLAY=:0.0 notify-send -u critical -t 5000 -i "$ICON" "Battery low at $PERCENTAGE%" "with $CAPACITY mah remaining, shutdown @ $CRITICAL_BATTERY mah"
         fi
