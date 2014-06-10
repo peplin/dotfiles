e6c6b0ecf5aa84892a73e54cc3d04f1d043e7d29
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-06-09 22:18:22 -0400

    Add workarounds for Java apps in Xmonad.

--- java/env.zsh
+++ java/env.zsh
@@ -1,6 +1,8 @@
 export M2_HOME=$HOME/bin/maven/
 export MAVEN_OPTS=-Xmx1512M
 export ANT_OPTS="-Xmx512m"
-export JAVA_OPTS="-Xmx512m"
-export JAVA_HOME="/usr/lib/jvm/default-java"
+export JAVA_OPTS="-Xmx1024m"
+#export JAVA_HOME="/usr/lib/jvm/default-java"
 export PATH=$PATH:$HOME/dev/processing/processing:$M2_HOME/bin
+export _JAVA_AWT_WM_NONREPARENTING=1
+export AWT_TOOLKIT=MToolkit
