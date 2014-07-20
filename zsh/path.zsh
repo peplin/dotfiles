480022194702c3fedfd200b4294f8729b341d08f
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-07-20 11:19:45 -0400

    Add dynamic colorscheme switching to urxvt.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,6 @@
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export PATH=$PATH:$HOME/dev/pebble/sdk/bin
+export PATH="$PATH:$HOME/.dynamic-colors/bin"
+source $HOME/.dynamic-colors/completions/dynamic-colors.zsh
 
 # add all first level subdirectories in ~/bin to PATH
 for DIR in `find ~/bin/ -maxdepth 1 -type d`; do
