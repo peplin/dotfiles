a1eb357d6342a46555080dda2d47d2b49a6646d7
Christopher Peplin <chris.peplin@rhubarbtech.com>
2016-01-15 11:59:24 -0500

    Don't bother with virtualenvwrapper.

--- python/env.zsh
+++ python/env.zsh
@@ -7,8 +7,6 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
 
-source `which virtualenvwrapper_lazy.sh`
-
 export NOSE_PROGRESSIVE_FUNCTION_COLOR=1
 export NOSE_PROGRESSIVE_DIM_COLOR=11
 export NOSE_PROGRESSIVE_BAR_FILLED_COLOR=2
