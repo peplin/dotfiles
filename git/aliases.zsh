e97918577256d3ab247380dc6e426ba913b46cbc
Christopher Peplin <cpeplin@aurora.tech>
2020-09-23 11:52:51 -0400

    New job, new branch!

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,4 @@
-alias git='hub'
+#alias git='hub'
 alias gitcheck="find . -maxdepth 1 -type d -exec sh -c 'echo {}; cd {} && git wtf; echo' \;"
 alias gitpurgelocal="git checkout master && git branch --merged | grep -v master | xargs git branch -d"
 alias gitpurgeremote="git checkout master && git remote prune origin && git branch -r --merged | grep -v master | sed -e 's/origin\\//:/' | xargs git push origin"
