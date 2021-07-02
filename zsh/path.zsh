78944f92430cdf0a0943848d255abd132477b515
Christopher Peplin <chris.peplin@rhubarbtech.com>
2021-07-02 12:18:40 -0400

    Don't bomb out on new systems without a full setup

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,12 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+if [[ -f $HOME/bin ]]; then
+    export PATH="$HOME/bin:$PATH"
+    # add all first level subdirectories in ~/bin to PATH
+    for DIR in `find ~/bin/ -maxdepth 1 -type d`; do
+        export PATH=$PATH:$DIR
+    done
+fi
+
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="$PATH:$HOME/.cabal/bin"
 export PATH="$PATH:$HOME/.npm/bin"
 
-# add all first level subdirectories in ~/bin to PATH
-for DIR in `find ~/bin/ -maxdepth 1 -type d`; do
-    export PATH=$PATH:$DIR
-done
