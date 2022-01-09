c28e7e973c3a157f9685774ffa69895e50445619
Christopher Peplin <chris.peplin@rhubarbtech.com>
2022-01-09 15:12:09 -0500

    Show commits ahead/behind with color

--- git/branch_tree.sh
+++ git/branch_tree.sh
@@ -8,6 +8,10 @@ set -e
 declare -A branch_parents
 outputs=()
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NO_COLOR="\033[0m"
+
 print_branch_tree() {
     local branch=$1
     local depth=($2)
@@ -32,9 +36,22 @@ print_branch_tree() {
     fi
 
     commits_diff=($(git rev-list --left-right --count $upstream...$branch))
-    local pretty_commit_count="(-${commits_diff[0]} +${commits_diff[1]})"
+    commits_ahead=$((commits_diff[1]))
+    commits_behind=$((commits_diff[0]))
+    local commits_output="("
 
-    outputs+=("$prefix$branch|$pretty_commit_count")
+    if (( commits_ahead > 0 && commits_behind == 0)); then
+        commits_output+="\033[0;32m+$commits_ahead\033[0m"
+    elif (( commits_ahead > 0 && commits_behind > 0)); then
+        commits_output+="\033[0;32m+$commits_ahead\033[0m, \033[0;31m-$commits_behind\033[0m"
+    elif (( commits_ahead == 0 && commits_behind > 0)); then
+        commits_output+="\033[0;31m+$commits_behind\033[0m"
+    else
+        commits_output+="0"
+    fi
+    commits_output+=")"
+
+    outputs+=("$prefix$branch|$commits_output")
 
     for child_branch in $child_branches; do
         print_branch_tree $child_branch $depth+1
@@ -47,6 +64,6 @@ build_branch_tree $branch_parents
 print_branch_tree $starting_branch 0
 
 for info in "${outputs[@]}"; do
-    echo "$info"
+    echo -e "$info"
 done | column -t -s "|"
 
