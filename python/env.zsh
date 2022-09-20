73568eaa509ae916529ec2f26a47ce111ce15f53
Christopher Peplin <chris.peplin@rhubarbtech.com>
2022-09-20 09:59:20 -0400

    Add local/bin to path

--- python/env.zsh
+++ python/env.zsh
@@ -5,5 +5,6 @@ export PROJECT_HOME=$PROJECTS
 export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export PIP_RESPECT_VIRTUALENV=true
+export PATH=$PATH:~/.local/bin/
 
 eval "$(pyenv init -)"
