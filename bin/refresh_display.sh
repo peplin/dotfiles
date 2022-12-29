8c1008d24f58b6466015721fc3aa59e464a9e4bc
Christopher Peplin <chris.peplin@rhubarbtech.com>
2022-12-29 18:37:25 -0500

    Refine xsettingsd

--- bin/refresh_display.sh
+++ bin/refresh_display.sh
@@ -5,13 +5,14 @@ autorandr -c
 xrdb -merge $HOME/.dotfiles/x/Xresources.base
 if [ $HOSTNAME == "X1C-PF3VJF1V" ]; then
     autorandr | grep current | grep mobile && xrdb -merge $HOME/.dotfiles/x/Xresources.laptop
-    xsettingsd --config $HOME/.dotfiles/x/xsettingsd-96 &
+    autorandr | grep current | grep mobile && xsettingsd --config $HOME/.dotfiles/x/xsettingsd-96 &
+    autorandr | grep current | grep docked && xsettingsd --config $HOME/.dotfiles/x/xsettingsd-120 &
 elif [ $HOSTNAME -eq "tangent" ]; then
     xrdb -merge $HOME/.dotfiles/x/Xresources.laptop
     xsettingsd --config $HOME/.dotfiles/x/xsettingsd-120 &
 fi
 
-killall -HUP xsettingsd
+#killall -HUP xsettingsd
 
 xmonad --restart
 
