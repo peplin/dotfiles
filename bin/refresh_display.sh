ec1bf2ccdb3c23d21fa8330cb19812823d56c5ea
Christopher Peplin <chris.peplin@rhubarbtech.com>
2022-12-29 11:25:34 -0500

    New mechanism for adjusting DPI on laptops

---
+++ bin/refresh_display.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+autorandr -c
+
+xrdb -merge $HOME/.dotfiles/x/Xresources.base
+if [ $HOSTNAME == "X1C-PF3VJF1V" ]; then
+    autorandr | grep current | grep mobile && xrdb -merge $HOME/.dotfiles/x/Xresources.laptop
+    xsettingsd --config $HOME/.dotfiles/x/xsettingsd-96 &
+elif [ $HOSTNAME -eq "tangent" ]; then
+    xrdb -merge $HOME/.dotfiles/x/Xresources.laptop
+    xsettingsd --config $HOME/.dotfiles/x/xsettingsd-120 &
+fi
+
+killall -HUP xsettingsd
+
+xmonad --restart
+
+xmodmap $HOME/.dotfiles/x/modcapslock
