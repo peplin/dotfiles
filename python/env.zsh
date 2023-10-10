09172dad66e6558b5edfef1b2ff4b3efdeaa4582
Christopher Peplin <chris.peplin@rhubarbtech.com>
2023-10-10 16:12:21 -0400

    Remove verrrry old python startup submodule

--- python/env.zsh
+++ python/env.zsh
@@ -1,6 +1,3 @@
-export PYTHONSTARTUP=$HOME/.dotfiles/python/startup/startup.py
-export PYTHONUSERDIR=$HOME/.dotfiles/python/startup
-
 export PROJECT_HOME=$PROJECTS
 export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
