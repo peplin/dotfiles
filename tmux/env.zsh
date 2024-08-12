330b0689a8ce9029d3ca1462847a8a501e7c4e1d
Christopher Peplin <chris.peplin@rhubarbtech.com>
2024-08-12 11:27:36 -0400

    tmux iterm integration for codesapces

--- tmux/env.zsh
+++ tmux/env.zsh
@@ -8,9 +8,17 @@ if [ $SSH_TTY ] && [ ! $WINDOW ] && [ ! $DISPLAY ]; then
         if which tmux >/dev/null 2>&1; then
             ID="`tmux ls | grep -vm1 attached | cut -d: -f1`" # get the id of a deattached session
             if [[ -z "$ID" ]] ;then # if not available create a new one
-                tmux new-session
+                if [ ! -z "$CODESPACES" ]; then
+                    tmux -CC
+                else
+                    tmux new-session
+                fi
             else
-                tmux attach-session -t "$ID" # if available attach to it
+                if [ ! -z "$CODESPACES" ]; then
+                    tmux -CC attach
+                else
+                    tmux attach-session -t "$ID" # if available attach to it
+                fi
             fi
         fi
     fi
