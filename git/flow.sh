2dfca8c1c181fee332c294cd36ff397085b5bdc4
Christopher Peplin <chris.peplin@rhubarbtech.com>
2024-11-08 13:25:49 -0500

    shlint

--- git/flow.sh
+++ git/flow.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIRECTORY="$(dirname "$0")"
-source $SCRIPT_DIRECTORY/util.sh
+source "$SCRIPT_DIRECTORY/util.sh"
 
 set -e
 
 declare -A branch_parents
-declare -A visited_branches
+declare visited_branches
 
 FORCE=""
 PUSH=""
@@ -24,44 +24,45 @@ done
 # Recursively rebase this and all child branches against their upstream branch.
 flowdown() {
     local branch=$1
-    local upstream=$(git rev-parse --abbrev-ref --symbolic-full-name $branch@{upstream})
+    local upstream
     local depth=$(($2))
     local push=$3
     local indentation=""
-    for i in $(seq 1 $depth); do
+    for _ in $(seq 1 $depth); do
         indentation+="  "
     done
 
+    upstream=$(git rev-parse --abbrev-ref --symbolic-full-name "$branch@{upstream}")
     visited_branches+=" $branch"
 
     # Use old school method when --update-refs fails due to a parent branch
     # having an amended commit.
     if [[ $depth == 0 ]] || [[ ${FORCE} == "true" ]]; then
         echo -n "${indentation}Rebasing $branch ..."
-        REBASE_OUTPUT=$(git rebase --quiet --fork-point $upstream $branch)
+        _=$(git rebase --quiet --fork-point $upstream $branch)
         echo "done"
     fi
 
     local child_branches=("${branch_parents[$branch]}")
     if [ -z "$child_branches" ] || [ ${#child_branches[@]} -eq 0 ]; then
         echo -n "Rebasing $branch and updating all parent refs..."
-        REBASE_OUTPUT=$(git rebase --update-refs $starting_branch $branch)
+        _=$(git rebase --update-refs "$starting_branch" "$branch")
         echo "done"
     fi
 
     for child_branch in $child_branches; do
-        flowdown $child_branch $depth+1 $push
+        flowdown "$child_branch" "$depth+1" "$push"
     done
 }
 
 find_current_branch
-find_starting_branch $BRANCH
+find_starting_branch "$BRANCH"
 build_branch_tree branch_parents
-flowdown $starting_branch 0
+flowdown "$starting_branch" 0
 
 if [[ ${PUSH} == "true" ]]; then
     echo "Pushing rebased branches..."
-    PUSH_OUTPUT=$(git push origin --force-with-lease $visited_branches)
+    _=$(git push origin --force-with-lease $visited_branches)
     echo "done"
 fi
 
