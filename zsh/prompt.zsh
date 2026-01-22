f97b6390ad51d2f97e52458566a1f2a60b3c55f4
Christopher Peplin <chris.peplin@rhubarbtech.com>
2026-01-22 15:15:05 -0500

    faster git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,72 +1,111 @@
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 autoload colors && colors
 
-git="/usr/bin/git"
+typeset -g _git_info=""
+typeset -g _git_info_pwd=""
+typeset -g _async_git_pid=0
 
-git_branch() {
-  echo $(timeout 0.1 $git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+_async_git_info() {
+    local dir="$1"
+    cd "$dir" 2>/dev/null || return
 
-git_prompt_info() {
-  ref=$(timeout 0.1 $git symbolic-ref HEAD 2> /dev/null) || return
-  echo "${ref#refs/heads/}"
-}
+    local git_dir
+    git_dir=$(git rev-parse --git-dir 2>/dev/null) || return
 
-git_dirty() {
-    st=$(timeout 0.2 $git --no-optional-locks status 2>/dev/null | tail -n 1)
-    if [[ $st == "" ]]
-    then
-        echo ""
+    local branch
+    branch=$(git symbolic-ref --short HEAD 2>/dev/null) || branch=$(git rev-parse --short HEAD 2>/dev/null) || return
+
+    local dirty=""
+    if ! git diff --quiet --ignore-submodules HEAD 2>/dev/null || \
+       [[ -n $(git status --porcelain 2>/dev/null | head -1) ]]; then
+        dirty="1"
+    fi
+
+    if [[ -n "$dirty" ]]; then
+        printf '\x1f%s\x1fred\x1f%s' "$branch" "$dir"
     else
-        if [[ "$st" =~ clean ]]
-        then
-            echo "%{$fg[green]%}$(git_branch)%{$reset_color%}"
-        else
-            echo "%{$fg_bold[red]%}$(git_branch)%{$reset_color%}"
-        fi
+        printf '\x1f%s\x1fgreen\x1f%s' "$branch" "$dir"
     fi
 }
 
-unpushed () {
-  timeout 0.1 $git cherry -v @{upstream} 2>/dev/null
+_handle_async_git() {
+    local output
+    read -r -u $1 output
+    zle -F $1
+    exec {1}<&-
+
+    if [[ $_async_git_pid -ne 0 ]]; then
+        _async_git_pid=0
+    fi
+
+    [[ -z "$output" ]] && return
+
+    local branch color dir
+    IFS=$'\x1f' read -r _ branch color dir <<< "$output"
+
+    [[ "$dir" != "$PWD" ]] && return
+
+    if [[ "$color" == "red" ]]; then
+        _git_info="%{$fg_bold[red]%}${branch}%{$reset_color%}"
+    else
+        _git_info="%{$fg[green]%}${branch}%{$reset_color%}"
+    fi
+    _git_info_pwd="$PWD"
+
+    zle && zle reset-prompt
 }
 
-project_name () {
-  name=$(pwd | awk -F'dev/' '{print $2}' | awk -F/ '{print $1}')
-  echo $name
+_start_async_git() {
+    if [[ $_async_git_pid -ne 0 ]]; then
+        kill $_async_git_pid 2>/dev/null
+        _async_git_pid=0
+    fi
+
+    local fd
+    exec {fd}< <(_async_git_info "$PWD")
+    _async_git_pid=$!
+    zle -F $fd _handle_async_git
 }
 
-project_name_color () {
-  name=$(project_name)
-  echo "%{\e[0;35m%}${name}%{\e[0m%}"
+_get_git_info() {
+    if [[ "$_git_info_pwd" == "$PWD" ]]; then
+        echo "$_git_info"
+    fi
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo "%{$fg_bold[red]%}+%{$reset_color%}"
-  fi
+project_name() {
+    local name
+    name=$(pwd | awk -F'dev/' '{print $2}' | awk -F/ '{print $1}')
+    echo $name
 }
 
-directory_name(){
-  echo "%{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}"
+project_name_color() {
+    local name
+    name=$(project_name)
+    echo "%{\e[0;35m%}${name}%{\e[0m%}"
 }
 
-aws_vault_profile () {
-  if [[ -n ${AWS_VAULT} ]]; then
-    echo "[AWS: ${AWS_VAULT}] "
-  fi
+directory_name() {
+    echo "%{$fg[green]%}${PWD/#$HOME/~}%{$reset_color%}"
+}
+
+aws_vault_profile() {
+    if [[ -n ${AWS_VAULT} ]]; then
+        echo "[AWS: ${AWS_VAULT}] "
+    fi
 }
 
 setopt PROMPT_SUBST
-export PROMPT=$'$(aws_vault_profile)$(directory_name) $(project_name_color)$(git_dirty)$(need_push)\n$ '
+export PROMPT=$'$(aws_vault_profile)$(directory_name) $(project_name_color)$(_get_git_info)\n$ '
 
 local return_code="%(?..%{$fg[red]%}%?%{$reset_color%})"
 export RPS1="${return_code}"
 
 precmd() {
-  title "zsh" "%55<...<%~"
+    title "zsh" "%55<...<%~"
+
+    if [[ "$_git_info_pwd" != "$PWD" ]]; then
+        _git_info=""
+    fi
+
+    _start_async_git
 }
