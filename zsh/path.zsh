4f37775cfde726bb042fd0c591a72f0c9fdcdab5
Christopher Peplin <chris.peplin@rhubarbtech.com>
2026-03-29 15:51:29 -0400

    simpler bin

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,10 +1,4 @@
-if [[ -d $HOME/bin ]]; then
-    export PATH="$HOME/bin:$PATH"
-    for DIR in ~/bin/*/; do
-        [[ -d "$DIR" ]] && export PATH="$PATH:$DIR"
-    done
-fi
-
+export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.local/bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
 export PATH="$HOME/.npm/bin:$PATH"
